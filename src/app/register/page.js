d3073cc5509dfcd13c5c0b6f1e7575db1ea38d46
RonakSingh98 <129096563+RonakSingh98@users.noreply.github.com>
2023-11-26 20:37:50 +0530

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from 'react';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
 import auth from '../../lib/firebase/firebase';
